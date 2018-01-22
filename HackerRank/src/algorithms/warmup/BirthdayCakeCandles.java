9625dfc9133313841d848a01af7b8295ae76dc31
BABAI <siddhantsambit@siddhantsambit@gmail.com>
2018-01-22 22:44:21 +0530

    WarmUp

--- HackerRank/src/algorithms/warmup/BirthdayCakeCandles.java
+++ HackerRank/src/algorithms/warmup/BirthdayCakeCandles.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package warmup;
+package algorithms.warmup;
 
 import java.util.Arrays;
 import java.util.Scanner;
--- HackerRank/src/algorithms/warmup/SimpleArraySum.java
+++ HackerRank/src/algorithms/warmup/SimpleArraySum.java
@@ -1,7 +1,7 @@
 /**
  * 
  */
-package warmup;
+package algorithms.warmup;
 
 import java.util.Scanner;
 
@@ -9,17 +9,17 @@ import java.util.Scanner;
  * @author BABAI
  *
  */
-public class AVeryBigSum {
+public class SimpleArraySum {
 	public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int arr[] = new int[n];
-        long sum = 0;
+        int sum = 0;
         for(int arr_i=0; arr_i < n; arr_i++){
             arr[arr_i] = in.nextInt();
         }
         for(int arr_i=0; arr_i < n; arr_i++){
-            sum = sum + arr[arr_i];
+            sum+= arr[arr_i];
         }
         System.out.println(sum);
         in.close();
